f961ebed01837f4773e296cd51daf58537b7997b
kouhei-sugahara <200092tw@yse-c.net>
2022-03-04 10:48:14 +0900

    animation修正

--- animation/animation.js
+++ animation/animation.js
@@ -3,22 +3,27 @@ console.log(monster)
 
 function show() {
     monster.fadeIn(2000)
+    //monster.slideDown()
 }
 
 function hide() {
-    monster.fadeOut(2000)
+    monster.fadeIn(1000)
 }
 
 function moveRight() {
+    monster.animate({left: 300}, 300)
 }
 
 function moveLeft() {
+    monster.animate({left: 0}, 300)
 }
 
 function moveUp() {
+    monster.animate({top: 50}, 300)
 }
 
 function moveDown() {
+    monster.animate({top: 300}, 300)
 }
 
 function moveSequence() {
