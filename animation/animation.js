4dd30fae372626c8bc75f7b3f5f10751486a86bb
kouhei-sugahara <200092tw@yse-c.net>
2022-03-04 10:31:57 +0900

    animation追加

---
+++ animation/animation.js
@@ -0,0 +1,25 @@
+var monster = $('#monster')
+console.log(monster)
+
+function show() {
+    monster.fadeIn(2000)
+}
+
+function hide() {
+    monster.fadeOut(2000)
+}
+
+function moveRight() {
+}
+
+function moveLeft() {
+}
+
+function moveUp() {
+}
+
+function moveDown() {
+}
+
+function moveSequence() {
+}
